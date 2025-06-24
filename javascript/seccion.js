03ac1c44fa830c3087ead27b3a495f05139acc0b
AlexSiv998 <alexrodriguez0398@gmail.com>
2025-06-24 19:48:42 -0400

    jv 1

---
+++ javascript/seccion.js
@@ -0,0 +1,37 @@
+//Esto es un comentario de una linea
+/*
+  comentario de multiples
+  lineas
+*/
+
+//variables
+/*
+  las variables son espacios de memoria que almacenan un valor
+*/
+//declaracion de una variable
+let nombre = "Alex"; // variable de tipo string
+console.log(nombre); // Imprime "Alex"
+//Reasignacion de una variable
+nombre = "Juan"; //reasignacion
+console.log(nombre); // Imprime "Juan"
+
+//tipos de datos
+let number = 42; // Numero
+let float = 3.14; // Numero de punto flotante (osea decimales)
+let string = "hola, mundo" // cadena de texo
+let boolean = true; // booleano solo puede ser (true/false)
+//let array = [1, 2, 3,]; // array (lista)
+let array = ["pera", "manzana", "uva",]; // lo mismo pero con string
+console.log(array); //imprime ["pera", "manzana", "uva",]
+console.log(array[7]); //imprime el primer elemento (0 es el elemto de inicio)
+let nulo = null; // nulo (sin valor)
+let indefinido; // undefined (sin asiginar)
+let objeto = {nombre: "Alex", edad: 30}; //objeto
+console.log(objeto); // imprime (nombre: "Alex", edad: 30)
+console.log(objeto.nombre); //imprime  "Alex"
+
+let suma = 5 + 3; // suma
+let resta = "a10" - "2"; // Resta
+
+//practica
+let prueba = "pedro"
